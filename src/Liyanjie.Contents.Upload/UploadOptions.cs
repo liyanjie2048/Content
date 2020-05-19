79e258f83ea30555d8f82efd5b01dbed758aa512
两颗番茄两块七 <liyanjie8712@outlook.com>
2020-05-19 10:29:52 +0800

    +

--- src/Liyanjie.Contents.Upload/UploadOptions.cs
+++ src/Liyanjie.Contents.Upload/UploadOptions.cs
@@ -11,13 +11,18 @@
         public string RootDirectory { get; set; }
 
         /// <summary>
-        /// 最大文件大小，单位：Byte
+        /// 最大文件大小。单位：Byte
         /// </summary>
         public long AllowedMaximumSize { get; set; } = 4294967296;
 
         /// <summary>
-        /// 允许的文件扩展名，默认：.jpg|.jpeg|.png|.gif
+        /// 允许的文件扩展名。默认：.jpg|.jpeg|.png|.gif
         /// </summary>
         public string AllowedExtensions { get; set; } = ".jpg|.jpeg|.png|.gif";
+        
+        /// <summary>
+        /// 文件名生成方案。默认：Guid
+        /// </summary>
+        public Func<string, string> FileNameScheme = origin => Guid.NewGuid().ToString("N");
     }
 }
